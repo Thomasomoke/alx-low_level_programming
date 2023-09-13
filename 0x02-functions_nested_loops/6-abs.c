740d893218b21a59b7650b8f045d544caed949ee
Thomasomoke <omokethomas644@gmail.com>
2023-09-13 04:34:44 -0700

    home

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * 6-abs.c  computes the absolute value of an integer
+ */
+int _abs(int)
+	int number;
+{
+	if (number < 0)
+	{
+		return (-number)
+	}
+	else
+	{
+		return (number);
+	}
+}
